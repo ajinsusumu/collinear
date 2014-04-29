4b7ef530fe789fc80852beee9188b7d1958c0a0e
ajinsusumu <ajinsusumu@gmail.com>
2014-04-29 18:22:08 +0800

    search sorted history to simplify logic.

--- Fast.java
+++ Fast.java
@@ -4,11 +4,13 @@ public class Fast {
     public static void main(String[] args) {
         String filename;
         if (args.length < 1) {
-//                          filename = "collinear/input50.txt";
-            filename = "collinear/rs1423.txt";
+            //                          filename = "collinear/input50.txt";
+            //            filename = "collinear/rs1423.txt";
+            //        filename = "collinear/input10.txt";
 //            filename = "collinear/input20.txt";
+            filename = "collinear/input50.txt";
             //            filename = "collinear/input8.txt";
-            //          filename = "collinear/input6.txt";
+            //                      filename = "collinear/input9.txt";
         } else {
             filename = args[0];
         }
@@ -31,24 +33,23 @@ public class Fast {
         double[] sl = new double[N];
         for (int i = 0; i < N - 3; i++) {
             Arrays.sort(p, i + 1, N, p[i].SLOPE_ORDER);
+            Arrays.sort(p, 0, i, p[i].SLOPE_ORDER);
 
             for (int j = 0; j < N; j++) sl[j] = p[i].slopeTo(p[j]);
             int count = 1;
+            int ii = 0;
             for (int k = i + 2; k < N; k++) {
                 if (sl[k - 1] == sl[k]) {
-                    count++;
+                    while (sl[ii] < sl[k] && ii < i) ii++;
+                    if (sl[ii] != sl[k]) count++;
                 } else {
-                    if ((count > 2) && !slope_in(0, i, sl[k - 1], sl)) segment_out(i, k - count, count, p, out);
+                    if (count > 2) segment_out(i, k - count, count, p, out);
                     count = 1;
                 }
             }
-            if ((count > 2) && !slope_in(0, i, sl[N - 1], sl)) segment_out(i, N - count, count, p, out);
+            if (count > 2) segment_out(i, N - count, count, p, out);
         }
         StdDraw.show(0);
-    }
-    static private boolean slope_in(int l, int h, double slope, double[] sl) {
-        for (int i = l; i < h; i++) if (sl[i] == slope) return true;
-        return false;
     }
     static private void segment_out(int o, int g, int count, Point[] q, Out out) {
         Point[] r = new Point[count + 1];
