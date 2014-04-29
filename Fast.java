5524792fca19f33e5986e85c550d2f7b0df97c15
ajinsusumu <ajinsusumu@gmail.com>
2014-04-29 16:43:11 +0800

    fix the redundant subsegment issue.

--- Fast.java
+++ Fast.java
@@ -4,12 +4,12 @@ public class Fast {
     public static void main(String[] args) {
         String filename;
         if (args.length < 1) {
-//        	filename = "collinear/input10.txt";
-        	filename = "collinear/rs1423.txt";
-//          String filename = "collinear/input8.txt";
-//          String filename = "collinear/input6.txt";
+                          filename = "collinear/input50.txt";
+//            filename = "collinear/rs1423.txt";
+            //            filename = "collinear/input8.txt";
+            //          filename = "collinear/input6.txt";
         } else {
-        	filename = args[0];
+            filename = args[0];
         }
         Out out = new Out();
 
@@ -34,18 +34,35 @@ public class Fast {
             int count = 1;
             int h = i + 1;
             for (int k = i + 2; k < N; k++) {
+                /*              if (p[i].slopeTo(p[i - 1]) == p[i].slopeTo(p[k - 1])) {
+                                break;
+                                }*/
                 if (p[i].slopeTo(p[k - 1]) == p[i].slopeTo(p[k])) {
                     count++;
                 } else {
-                    if (count > 2) { segment_out(i, h, k - count, count, p, out); h += count; }
+                    if (count > 2) {
+                        if (!slope_in(0, i, i, p[i].slopeTo(p[k - 1]), p)) {
+                            segment_out(i, h, k - count, count, p, out); h += count;
+                        }
+                    }
                     count = 1;
                 }
             }
-            if (count > 2) { segment_out(i, h, N - count, count, p, out); h += count; }
+            if (count > 2) {
+                if (!slope_in(0, i, i, p[i].slopeTo(p[N - 1]), p)) {
+                    segment_out(i, h, N - count, count, p, out); h += count;
+                }
+            }
             //i = h;
             i++;
         }
         StdDraw.show(0);
+    }
+    static private boolean slope_in(int l, int h, int o, double slope, Point[] p) {
+        for (int i = l; i < h; i++) {
+            if (p[o].slopeTo(p[i]) == slope) return true;
+        }
+        return false;
     }
     static private void segment_out(int o, int h, int g, int count, Point[] q, Out out) {
         Point[] r = new Point[count + 1];
@@ -55,14 +72,13 @@ public class Fast {
         for (int l = 0; l < count; l++) out.printf("%s -> ", r[l]);
         out.printf("%s%n",r[count]);
         r[0].drawTo(r[count]);
-        
 
         /* move _count_ Points starting from g to o+1*/
         /*for (int i = g + count - 1, j = h; i >= g && j < g; i--, j++) {
-            Point t = q[i];
-            q[i] = q[j];
-            q[j] = t;
-        }*/
+          Point t = q[i];
+          q[i] = q[j];
+          q[j] = t;
+          }*/
     }
     static private void segment_out1(int g, int count, Point[] q, Out out) {
         Point[] r = new Point[count+1];
