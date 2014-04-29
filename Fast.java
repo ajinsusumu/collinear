ecbc04b0acada501ae0831312a33c382dfc3418c
ajinsusumu <ajinsusumu@gmail.com>
2014-04-29 14:49:08 +0800

    1. no miss lines. 2. side effect that subsegments not eliminated.

--- Fast.java
+++ Fast.java
@@ -4,6 +4,7 @@ public class Fast {
     public static void main(String[] args) {
         String filename;
         if (args.length < 1) {
+//        	filename = "collinear/input10.txt";
         	filename = "collinear/rs1423.txt";
 //          String filename = "collinear/input8.txt";
 //          String filename = "collinear/input6.txt";
@@ -41,7 +42,8 @@ public class Fast {
                 }
             }
             if (count > 2) { segment_out(i, h, N - count, count, p, out); h += count; }
-            i = h;
+            //i = h;
+            i++;
         }
         StdDraw.show(0);
     }
@@ -56,11 +58,11 @@ public class Fast {
         
 
         /* move _count_ Points starting from g to o+1*/
-        for (int i = g + count - 1, j = h; i >= g && j < g; i--, j++) {
+        /*for (int i = g + count - 1, j = h; i >= g && j < g; i--, j++) {
             Point t = q[i];
             q[i] = q[j];
             q[j] = t;
-        }
+        }*/
     }
     static private void segment_out1(int g, int count, Point[] q, Out out) {
         Point[] r = new Point[count+1];
